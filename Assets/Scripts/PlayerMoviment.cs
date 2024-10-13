95e5f2ab6def33d7b6b69b6705cd0f6688bbe3df
Arthur Matos de Souza <79379167+Ursidios@users.noreply.github.com>
2024-10-13 14:24:34 -0300

    feat(occlusion culling)

---
+++ Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private Rigidbody2D rb;
+    public bool isOnFloor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Move();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Update()
+    {
+        if(Input.GetButtonDown("Jump"))
+            Jump();
+    }
+
+    public float GetXDirection()
+    {
+        float x = Input.GetAxis("Horizontal");    
+
+        return x;
+    }
+    public float GetYDirection()
+    {
+        float y = Input.GetAxis("Vertical");
+
+        return y;
+    }
+
+    public void Move()
+    {
+        rb.AddForce(Vector2.right * GetXDirection()  * speed, ForceMode2D.Force);
+    }
+    
+    public void Jump()
+    {
+        if(isOnFloor)
+        {
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Force) ;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        Vector2 pontoDeContato = collision.contacts[0].point;
+
+        Vector2 posicaoDoObjeto = transform.position;
+
+        if (pontoDeContato.y < posicaoDoObjeto.y)
+        {
+           isOnFloor = true;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        isOnFloor = false;
+    }
+}
